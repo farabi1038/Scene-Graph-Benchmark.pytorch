07d1353e3b7274c95df9595c46cf582c58f41787
Huan <gladcolor@gmail.com>
2020-09-22 12:28:52 -0400

    changed train_run.sh

--- train_run.sh
+++ train_run.sh
@@ -1,2 +1,3 @@
 df
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch  --master_port 10001  --nproc_per_node=2 tools/detector_pretrain_net.py --config-file "configs/e2e_relation_detector_X_101_32_8_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 4 TEST.IMS_PER_BATCH 2 DTYPE "float16" SOLVER.MAX_ITER 50000 SOLVER.STEPS "(30000, 45000)" SOLVER.VAL_PERIOD 2000 SOLVER.CHECKPOINT_PERIOD 2000 MODEL.RELATION_ON False OUTPUT_DIR output/checkpoints/pretrained_faster_rcnn/ SOLVER.PRE_VAL False
+nvidia-smi
+CUDA_VISIBLE_DEVICES=0,1,3,4 python -m torch.distributed.launch  --master_port 10001  --nproc_per_node=4 tools/detector_pretrain_net.py --config-file "configs/e2e_relation_detector_X_101_32_8_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 4 TEST.IMS_PER_BATCH 2 DTYPE "float16" SOLVER.MAX_ITER 50000 SOLVER.STEPS "(30000, 45000)" SOLVER.VAL_PERIOD 2000 SOLVER.CHECKPOINT_PERIOD 2000 MODEL.RELATION_ON False OUTPUT_DIR output/checkpoints/pretrained_faster_rcnn/ SOLVER.PRE_VAL False
