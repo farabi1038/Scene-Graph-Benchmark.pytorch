8d8a2370c3657b3afba5098f11f3461a2e2ea18f
Huan <gladcolor@gmail.com>
2020-09-23 12:50:30 -0400

    added relation_train_run.sh

--- relation_train_run.sh
+++ relation_train_run.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python -m torch.distributed.launch --master_port 10025 --nproc_per_node=4 tools/relation_train_net.py \
 --config-file "configs/e2e_relation_X_101_32_8_FPN_1x.yaml" \
-MODEL.ROI_RELATION_HEAD.USE_GT_BOX False  \
-MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL False  \
+MODEL.ROI_RELATION_HEAD.USE_GT_BOX True  \
+MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL True  \
 MODEL.ROI_RELATION_HEAD.PREDICTOR CausalAnalysisPredictor  \
 MODEL.ROI_RELATION_HEAD.CAUSAL.EFFECT_TYPE none  \
 MODEL.ROI_RELATION_HEAD.CAUSAL.FUSION_TYPE sum  \
